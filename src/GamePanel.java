4966dfe9454a0f9e01175def6f8717adb32a58f0
vcaf <victoria.fong.0408@gmail.com>
2022-07-11 14:27:52 +0200

    created needed classes and added methods and initial valjues for the game to start

---
+++ src/GamePanel.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = ((SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE);
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS] ;
+    final int y[] = new int[GAME_UNITS] ;
+    int bodyParts = 6;
+    int applesEaten = 0;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+    GamePanel(){
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
+    }
+    public void startGame() {
+
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+    public void newApple(){
+
+    }
+    public void move() {
+
+    }
+    public void checkApple() {
+
+    }
+    public void checkCollisions() {
+
+    }
+    public void gameOver(Graphics g) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+    }
+}
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,5 +1,7 @@
-public class Main {
+public class SnakeGame {
     public static void main(String[] args) {
+
+        new GameFrame();
         System.out.println("Hello world!");
     }
 }
